aa095a2b9d4a4531387ad8a01f0e0444bb790337
William <will.liiam06@gmail.com>
2020-05-07 19:16:20 +0700

    initial project

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by William on 07/05/20.
+//  Copyright © 2020 William. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
